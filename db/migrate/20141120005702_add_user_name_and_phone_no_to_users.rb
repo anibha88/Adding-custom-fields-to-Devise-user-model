0d221b8532dba69b8a710caab894fddce7db29cf
Nithin-CC <anibha88@gmail.com>
2014-11-20 13:10:57 +0530

    Adding other fields

---
+++ db/migrate/20141120005702_add_user_name_and_phone_no_to_users.rb
@@ -0,0 +1,6 @@
+class AddUserNameAndPhoneNoToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :phone_no, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119134705) do
+ActiveRecord::Schema.define(version: 20141120005702) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -28,6 +28,10 @@ ActiveRecord::Schema.define(version: 20141119134705) do
     t.datetime "updated_at"
     t.string   "first_name"
     t.string   "last_name"
+    t.date     "birthday"
+    t.string   "gender"
+    t.string   "username"
+    t.integer  "phone_no"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
